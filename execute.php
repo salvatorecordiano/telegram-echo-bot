33693cf26047e4b824d8564312a1d7a85ec1c7cc
Mike Tamanti <mike.tamanti@gmail.com>
2018-05-03 16:47:41 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -55,7 +55,7 @@ if(isset($message['text']))
 	$pollicesym =  json_decode('"\uD83D\uDC4D"');
 	$worldsym = json_decode('"\uD83C\uDF0F"');
 	$obj_desc = $text_url_array[0];
-	$response = "Ecco fatto: $obj_desc\n$worldsym  $url_affiliate . DominioA: $dominioAmazon";
+	$response = "Ecco fatto: $obj_desc\n$worldsym  $url_affiliate . DominioA: $dominioAmazon . DOminioG: $dominioGearbest";
 	
   }
    elseif($dominioGearbest == "gearbest")

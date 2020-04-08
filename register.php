276c01d34b42609a4a62da60657cf944af3d7af6
Salvatore Cordiano <salvatorecordiano@users.noreply.github.com>
2020-04-08 20:15:15 +0200

    Update register.php

--- register.php
+++ register.php
@@ -5,13 +5,12 @@ $WEBHOOK_URL = 'https://{PROJECT NAME}.000webhostapp.com/webhook.php';
 $BOT_TOKEN = '{TOKEN}';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
-$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
-$method = 'setWebhook';
 $parameters = array('url' => $WEBHOOK_URL);
-$url = $API_URL . $method. '?' . \http_build_query($parameters);
+$url = \sprintf('https://api.telegram.org/bot%s/setWebhook?%s', $BOT_TOKEN, \http_build_query($parameters));
 $handle = \curl_init($url);
 \curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 \curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
 \curl_setopt($handle, CURLOPT_TIMEOUT, 60);
 $result = \curl_exec($handle);
+\curl_close($handle);
 \print_r($result);
